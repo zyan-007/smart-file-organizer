2d935cf8374f1cf7e0a166fe28234dfe57ab4a9c
zyan-007 <zyanprogramming@gmail.com>
2026-04-05 17:07:58 +0530

    Add config file with file type categories

---
+++ config.py
@@ -0,0 +1,7 @@
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt", ".pptx"],
+    "Videos": [".mp4", ".mkv", ".avi"],
+    "Audio": [".mp3", ".wav"],
+    "Code": [".py", ".cpp", ".js", ".html", ".css"],
+}
