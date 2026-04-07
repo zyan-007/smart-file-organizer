f7c67dea28a40b88755652c7f2a9de20c842835c
zyan-007 <zyanprogramming@gmail.com>
2026-04-07 19:26:02 +0530

    Refactor utilities to use dynamic categories from config

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 import os
-from config import FILE_CATEGORIES
 
-def get_category(file_name):
+def get_category(file_name, categories):
     _, ext = os.path.splitext(file_name)
     ext = ext.lower()
 
-    for category, extensions in FILE_CATEGORIES.items():
+    for category, extensions in categories.items():
         if ext in extensions:
             return category
 
     return "Others"
+
 
 def log_action(message):
     with open("organizer.log", "a") as f:
