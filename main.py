7205e0123ae17023d28a03f5fb00a5550a50eabb
zyan-007 <zyanprogramming@gmail.com>
2026-04-07 19:30:04 +0530

    Add CLI support for undo operation

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import argparse
 from organizer import organize_files
+from undo import undo_last_operation
 
 
 def main():
     parser = argparse.ArgumentParser(description="Smart File Organizer")
 
-    parser.add_argument("path", help="Folder path to organize")
+    parser.add_argument("path", nargs="?", help="Folder path to organize")
     parser.add_argument("--dry-run", action="store_true", help="Preview changes")
     parser.add_argument("--recursive", action="store_true", help="Include subfolders")
+    parser.add_argument("--undo", action="store_true", help="Undo last operation")
 
     args = parser.parse_args()
+
+    if args.undo:
+        undo_last_operation()
+        return
+
+    if not args.path:
+        print("Please provide a path or use --undo")
+        return
 
     organize_files(
         args.path,
