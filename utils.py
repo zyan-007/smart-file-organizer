c1d9a11bcdfc0ca3a29de6ae78d723167a4fb15e
zyan-007 <zyanprogramming@gmail.com>
2026-04-10 20:01:37 +0530

    Add automatic renaming for duplicate files instead of skipping

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 
+
 def get_category(file_name, categories):
     _, ext = os.path.splitext(file_name)
     ext = ext.lower()
@@ -14,3 +15,18 @@ def get_category(file_name, categories):
 def log_action(message):
     with open("organizer.log", "a") as f:
         f.write(message + "\n")
+
+
+def get_unique_filename(path):
+    """
+    If file exists, append (1), (2), etc.
+    """
+    base, ext = os.path.splitext(path)
+    counter = 1
+
+    new_path = path
+    while os.path.exists(new_path):
+        new_path = f"{base}({counter}){ext}"
+        counter += 1
+
+    return new_path
